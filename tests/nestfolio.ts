8f986283c8f4a2c023bcc60550fc17b0938e76f2
AvhiMaz <avhimazumder5@outlook.com>
2025-02-13 20:22:37 +0530

    test: add comprehensive testcase for DAO initialization

--- tests/nestfolio.ts
+++ tests/nestfolio.ts
@@ -3,6 +3,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PublicKey } from "@solana/web3.js";
 import { Nestfolio } from "../target/types/nestfolio";
+import { expect } from "chai";
 
 const IDL = require("../target/idl/nestfolio.json");
 
@@ -19,5 +20,29 @@ describe("Nestfolio DAO Initialization", () => {
     );
     const provider = new BankrunProvider(context);
     const daoProgram = new Program<Nestfolio>(IDL, provider);
+
+    const creator = provider.wallet.publicKey;
+
+    await daoProgram.methods
+      .initializeOrganization("DAO", new anchor.BN(1000))
+      .rpc();
+
+    const [daoAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("organization"), creator.toBuffer()],
+      DAO_PROGRAM_ID
+    );
+
+    const dao = await daoProgram.account.organisation.fetchNullable(daoAddress);
+
+    console.log(dao);
+
+    expect(dao.name).to.equal("DAO");
+    expect(dao.treasuryBalance.toString()).to.equal("0");
+    expect(dao.totalMembers).to.equal(0);
+    expect(dao.createdAt.toString()).to.not.equal(0);
+    expect(dao.status).to.be.true;
+    expect(dao.proposalLimit).to.equal(10);
+    expect(dao.memberRegistrationFee.toString()).to.equal("1000");
+    expect(dao.minimumDepositAmount.toString()).to.equal("1000");
   });
 });
