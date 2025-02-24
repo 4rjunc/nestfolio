a9b3f5d53e309c7cac8d294fe095b1d9724d59ea
AvhiMaz <avhimazumder5@outlook.com>
2025-02-24 12:25:32 +0530

    feat: implement create proposal contract
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- programs/nestfolio/src/instructions/mod.rs
+++ programs/nestfolio/src/instructions/mod.rs
@@ -1,9 +1,11 @@
+pub mod create_proposal;
 pub mod desposit_fund;
 pub mod init_member;
 pub mod init_org;
 pub mod update_org;
 pub mod withdraw_fund;
 
+pub use create_proposal::*;
 pub use desposit_fund::*;
 pub use init_member::*;
 pub use init_org::*;
