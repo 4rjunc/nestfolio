622fa59341d52cab040133be08a7216d6b93e896
4rjunc <arjunmundamani@gmail.com>
2025-03-02 11:25:07 +0530

    Added resume

--- aiagent/bot.js
+++ aiagent/bot.js
@@ -1,7 +1,7 @@
 import { Bot } from "grammy";
 import 'dotenv/config'
-import { initDAO } from './program.js'; 
-import { analyzeDAOInit, emergencyPause } from "./prompt.js"
+import { initDAO, emergencyPause } from './program.js'; 
+import { analyzeDAOInit } from "./prompt.js"
 
 const token = process.env.BOT_API_KEY;
 const bot = new Bot(token); 
@@ -30,5 +30,18 @@ bot.command("DAOpause", async (ctx) => {
 //  { command: "start", description: "Start the bot" },
 //  { command: "createDAO", description: "To Create DAO" },
 //]);
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
 
 bot.start();
